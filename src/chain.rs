cf0b684b14c5ec1547d019482cc6269b53afbe52
Min <minhnguyen1995@gmail.com>
2024-10-22 16:21:21 +0100

    Corrected bug in updating the endpoint_hash (to block.hash) for new forks

--- src/chain.rs
+++ src/chain.rs
@@ -87,8 +87,6 @@ impl Chain {
     pub fn handle_new_block(&mut self, block: Block) -> Result<NextBlockResult, NextBlockErr>{
         Block::validate_block(&block)?;
 
-        let endpoint_idx = block.idx;
-        let endpoint_hash = block.hash.clone();
         // Search for the parent block in the main chain.
         if let Some(parent_block) = Block::find(&self.main, &block.prev_hash){
 
@@ -99,8 +97,8 @@ impl Chain {
                 Block::push(&mut self.main, &block);
                 Ok(NextBlockResult::ExtendedMain {
                         length: self.len(),
-                        endpoint_idx,
-                        endpoint_hash
+                        endpoint_idx: block.idx,
+                        endpoint_hash: block.hash
                     })
             }
             // Otherwise attach a single-block fork to the main chain
@@ -113,37 +111,37 @@ impl Chain {
                     length: 1,
                     forkpoint_idx: block.idx - 1,
                     forkpoint_hash: block.prev_hash,
-                    endpoint_idx,
-                    endpoint_hash
+                    endpoint_idx: block.idx,
+                    endpoint_hash: block.hash
                 })
             }
         }
         // Search for the parent block in the forks.
-        else if let Some((  forkpoint_hash,
-                            endpoint_hash,
+        else if let Some((  forkpoint,
+                            endpoint,
                             fork)) = self.find_fork_mut( &block.prev_hash) {
             let parent_block = Block::find(fork, &block.prev_hash).unwrap();
 
             Block::validate_child(parent_block, &block)?;
 
             // If its parent was the last block in the fork, append the block to the fork
-            if endpoint_hash == parent_block.hash {
+            if endpoint == parent_block.hash {
                 // Update the endpoint_hash of the extended fork in the map.
                 let extended_fork: &Vec<Block> = {
                     Block::push(fork, &block);
-                    self.forks.entry(forkpoint_hash.clone()).and_modify(|forks| {
-                        let fork: Vec<Block> = forks.remove(&endpoint_hash).expect("fork definitely exists.");
+                    self.forks.entry(forkpoint.clone()).and_modify(|forks| {
+                        let fork: Vec<Block> = forks.remove(&endpoint).expect("fork definitely exists.");
                         forks.insert(block.hash.clone(), fork.clone());
                     });
-                    self.forks.get(&forkpoint_hash).unwrap().get(&block.hash).unwrap()
+                    self.forks.get(&forkpoint).unwrap().get(&block.hash).unwrap()
                 };
                 println!("Extending an existing fork");
                 Ok(NextBlockResult::ExtendedFork {
                     length: extended_fork.len(),
-                    forkpoint_idx: extended_fork.first().expect("fork is non-empty").idx - 1,
-                    forkpoint_hash,
-                    endpoint_idx,
-                    endpoint_hash
+                    forkpoint_idx: extended_fork.first().unwrap().idx - 1,
+                    forkpoint_hash: forkpoint,
+                    endpoint_idx: block.idx,
+                    endpoint_hash: block.hash
                 })
             }
             // Otherwise create a new direct fork from the main chain, whose prefix is a clone of an existing fork's, with
@@ -157,16 +155,16 @@ impl Chain {
                 };
 
                 // Insert the new fork into the map.
-                self.forks.entry(forkpoint_hash.clone()).and_modify(|forks: &mut HashMap<String, Vec<Block>>| {
+                self.forks.entry(forkpoint.clone()).and_modify(|forks: &mut HashMap<String, Vec<Block>>| {
                     forks.insert(block.hash.clone(), new_fork.clone());
                 });
 
                 Ok(NextBlockResult::NewFork {
                     length: new_fork.len(),
-                    forkpoint_idx: new_fork.first().expect("fork is non-empty").idx - 1,
-                    forkpoint_hash,
-                    endpoint_idx,
-                    endpoint_hash
+                    forkpoint_idx: new_fork.first().unwrap().idx - 1,
+                    forkpoint_hash: forkpoint,
+                    endpoint_idx: block.idx,
+                    endpoint_hash: block.hash
                 })
             }
         }
